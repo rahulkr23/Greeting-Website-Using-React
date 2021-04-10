f1ab785cb595e2ac06da5b0aa28c2cb55d8b942d
rahulkr23 <rahulwhy23@gmail.com>
2021-04-10 16:14:51 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+
+let currDate = new Date(2021, 4, 5, 8);
+currDate = currDate.getHours();
+let greeting = '';
+
+const cssStyle = { };
+
+if(currDate >= 1 && currDate <12){
+  greeting = 'Good Morning🌅, Rahul';
+  cssStyle.color = 'green';
+
+}else if(currDate>=12 && currDate <19){
+  greeting = "Good Afternoon🌕, Rahul";
+  cssStyle.color = 'orange';
+}else{
+  greeting = "Good night🌆, Rahul";
+  cssStyle.color = 'black';
+}
+
+
+ReactDOM.render(
+
+<>
+<div>
+  <h1>Hello 😁 👋, <span style={cssStyle}> {greeting} </span>  </h1>
+  </div>
+  </>
+          ,document.getElementById('root')
+
+
+);
+
